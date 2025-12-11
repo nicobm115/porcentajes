d01b01ee88a678216e2445434c182694af8bf1ee
nicobm115 <nicodarg007@gmail.com>
2025-12-11 11:47:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ with st.expander("➕ Añadir Nuevo Ingreso", expanded=True):
     c1, c2,c3 = st.columns(3)
     
     with c1:
-        bruto = st.number_input("Importe Bruto ", min_value=0.0, step=10.0,value=60)
+        bruto = st.number_input("Importe Bruto ", min_value=0, step=10,value=60)
     with c2:
         # El usuario introduce SU porcentaje
-        porc_usuario = st.number_input("Tu Porcentaje (%)", min_value=0.0, max_value=100.0, value=70)
+        porc_usuario = st.number_input("Tu Porcentaje (%)", min_value=0, max_value=100, value=70)
     with c3:
         Nota = st.text_input("Nota ")
 
@@ -94,7 +94,6 @@ if len(st.session_state['registros']) > 0:
         st.balloons('te quiero 🌻')
 else:
     st.info("No hay registros todavía.")
-
 
 
 
